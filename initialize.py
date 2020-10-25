9bc2a210f811cdf508e2c85bb772a38e50aeda61
Alfred Wrigfors <alfred.wrigfors@gmail.com>
2020-10-25 02:56:01 +0100

    RDIF-implemtation

--- initialize.py
+++ initialize.py
@@ -1,71 +1,33 @@
-# Import libraries
-from imutils.video import VideoStream
-import cv2
-import numpy as np
-from pyzbar import pyzbar
-import argparse
+# Import classes/libraries
 import datetime
 import time
-import imutils
+from gpiozero import LED, Button
+import RPi.GPIO as gpiozero
+from mfrc522 import SimpleMFRC5222
 
-# Store the data from the last QR-code to not re-authenticate the same user again
-last_found = ""
+# Import custom classes/libraries
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
-	help="path to output CSV file containing barcodes")
-args = vars(ap.parse_args())
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
-# open the output CSV file for writing and initialize the set of
-# barcodes found thus far
-csv = open(args["output"], "w")
-found = set()
+# Declaration of hardware
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+# LEDs
+error_LED = LED(21)
+running_LED = LED(20)
+status_LED = ["ready"=> LED(6), "detected"=> LED(5), "read"=> LED(24), "allowed"=> LED(25)]
 
-# loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream and resize it to
-    # have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=1000)
-    # find the barcodes in the frame and decode each of the barcodes
-    barcodes = pyzbar.decode(frame)
-    # loop over the detected barcodes
-    for barcode in barcodes:
-        # extract the bounding box location of the barcode and draw
-        # the bounding box surrounding the barcode on the image
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        # the barcode data is a bytes object so if we want to draw it
-        # on our output image we need to convert it to a string first
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
+# Buttons
+exit_Butoon = Button(17)
+deny_Button = Button(27)
+allow_Button = Button(22)
 
-        # draw the barcode data and barcode type on the image
-        text = "{} ({})".format(barcodeData, barcodeType)
+# Setup
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+# RFID-reader
+rfid_reader = SimpleMFRC5222()
 
-        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        # if the barcode text is currently not in our CSV file, write
-        		# the timestamp + barcode to disk and update the set
-        if barcodeData != last_found:
-            csv.write("{},{}\n".format(datetime.datetime.now(), barcodeData))
-            csv.flush()
-            last_found = barcodeData
-            print(barcodeData)
-    # show the output frame
-    cv2.imshow("Barcode Scanner", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-# close the output CSV file do a bit of cleanup
-print("[INFO] cleaning up...")
-csv.close()
-cv2.destroyAllWindows()
-vs.stop()
+try:
+	id, text = rfid_reader.read()
+	print(id)
+	print(text)
+finally:
+	GPIO.cleanup()
