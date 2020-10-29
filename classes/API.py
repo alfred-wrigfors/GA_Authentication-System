47eaa53742994c36d1d85c38c3c44c599f5f63e3
Alfred Wrigfors <alfred.wrigfors@gmail.com>
2020-10-29 23:43:07 +0100

    Updated management

--- classes/API.py
+++ classes/API.py
@@ -1,16 +1,29 @@
-class API:
-    # importing the requests library
-    import requests
+class API_class:
 
-    def __init__():
-        pass
+    def __init__(empty, device_id, api_domain, username, password):
+        import requests
+        self.device_id = device_id
+        self.api_domain = api_domain
+        self.username = username
+        self.password = password
 
-    def POST():
-        pass
 
-    def GET(URL, PARAMS):
-        # sending get request and saving the response as response object
-        r = requests.get(url = URL, params = PARAMS)
+    def authenticate(empty, card_id,):
+        import requests
+        import json
 
-        # return data in json format
-        return r.json()
+        payload = {"card_id": str(card_id), "device_id":self.device_id}
+        r = requests.get(self.api_domain, params=payload)
+        x = r.json()
+
+        print(x["message"])
+        if x["valid"] == True:
+            if x["authorized"] == True:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def send_offline_state():
+        print("NOOOOOO")
--- initialize.py
+++ initialize.py
@@ -1,33 +1,72 @@
 # Import classes/libraries
-import datetime
+from gpiozero import LED, LEDBoard
+import RPi.GPIO as GPIO
+
+from mfrc522 import SimpleMFRC522
+
 import time
-from gpiozero import LED, Button
-import RPi.GPIO as gpiozero
-from mfrc522 import SimpleMFRC5222
+import atexit
+import requests
+import json
 
 # Import custom classes/libraries
+from classes.API import API_class
 
+# Declaration of variables
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Read data from config.json
+with open('config.json') as json_file:
+	# API
+	API = API_class(json_file['device_id'], json_file['api_domain'], json_file['api_credentials']['username'], json_file['api_credentials']['password'])
 
 # Declaration of hardware
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------
 # LEDs
-error_LED = LED(21)
-running_LED = LED(20)
-status_LED = [LED(6), LED(5), LED(24), LED(25)]
+error_LED = LED("GPIO27")
+allowed_LED = LEDBoard("GPIO17")
+armed_LED = LED("GPIO22")
 
-# Buttons
-exit_Butoon = Button(17)
-deny_Button = Button(27)
-allow_Button = Button(22)
+# Defone functions
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# When the user is authorized
+def access_allowed():
+	allowed_LED.on()
+
+# When the user isn't authorized
+def access_denied():
+	error_LED.on()
+
+# Restore parameters to ready-state
+def access_restore():
+	time.sleep(0.5)
+	error_LED.off()
+	allowed_LED.off()
+
 
 # Setup
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------
 # RFID-reader
-rfid_reader = SimpleMFRC5222()
+rfid_reader = SimpleMFRC522()
+armed_LED.on()
+GPIO.setmode(GPIO.BCM)
 
-try:
-	id, text = rfid_reader.read()
-	print(id)
-	print(text)
-finally:
-	GPIO.cleanup()
+while True:
+
+	try:
+		id, text = rfid_reader.read()
+
+	finally:
+		pass
+
+	if API.authenticate(id, 3):
+		access_allowed()
+
+	else:
+		access_denied()
+
+
+	access_restore()
+
+# Upon exit, send-offline-state to API
+atexit.register(API.send_offline_state)
